937c5b8b405c73d93295b12ad193e91cd9e53b11
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-09-20 19:48:36 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,8 @@ class PostsController < ApplicationController
 	def edit
 	  @post = Post.find(params[:id])
 	end
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
 end
